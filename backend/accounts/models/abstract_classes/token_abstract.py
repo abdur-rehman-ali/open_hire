c024ea345b4f9316c910ee55db2de2279bc63fcb
abdur-rehman-ali <abdurrehmanali369@gmail.com>
2026-05-30 22:51:11 -0700

    Fix: Update is_expired method to include is_used check

--- backend/accounts/models/abstract_classes/token_abstract.py
+++ backend/accounts/models/abstract_classes/token_abstract.py
@@ -35,7 +35,7 @@ class TokenAbstract(models.Model):
         ).delete()
 
     def is_expired(self):
-        return timezone.now() > self.expires_at
+        return self.is_used or timezone.now() > self.expires_at
 
     def __str__(self):
         return f"{self.__class__.__name__} for {self.user.username}"
